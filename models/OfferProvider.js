af82f913ad4a68504b8ca033a9277bbf1c7b2fe0
Gordon Freeman <drfr33man@protonmail.com>
2021-09-16 11:23:03 +0300

    change offer api

---
+++ models/OfferProvider.js
@@ -0,0 +1,26 @@
+const db = require("./../database");
+const { DataTypes } = require("sequelize");
+const OfferProvider = db.define(
+  "Postback",
+  {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true
+    },
+
+    name: {
+      type: DataTypes.STRING
+      // allowNull defaults to true
+    },
+    lastUpdate: {
+      type: DataTypes.DATE
+      // allowNull defaults to true
+    }
+  },
+  {
+    // Other model options go here
+  }
+);
+module.exports = OfferProvider;
--- routes/api/offers.js
+++ routes/api/offers.js
@@ -3,58 +3,68 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 //Item Model
 const Offer = require("../../models/Offer");
+const OfferProvider = require("../../models/OfferProvider");
 
 // @route GET api/items
 // @desc Get All Items
 // @acces Public
+
+//cpalead provider
+const url = new URL(
+  "http://cpalead.com/dashboard/reports/campaign_json.php?id=1721323"
+);
+
 router.post("/", auth, (req, res) => {
   const { subid, offer_type, country, device } = req.body;
 
-  // #1 Update offers database by calling offer providers if necessary 
-  // OfferProvider.findOne({where:{name:"cpalead"}})
-  //.then(p=>console.log(p.lastUpdate))
-  // cpalead
-  // kiwi
+  // #1 Update offers database by calling offer providers if necessary
+  OfferProvider.findOne({ where: { name: "cpalead" } }).then(p => {
+    const curr_date = +Date();
+    const last_date = p.lastUpdate;
+    const diffMins = Math.round(
+      (((curr_date - last_date) % 86400000) % 3600000) / 60000
+    ); // minutes
+    console.log(diffMins);
+    if (diffMins > 10) {
+      axios
+        .get(url)
+        .then(res => {
+          let offer_arr = [];
+          res.data.offers.map(
+            ({
+              title,
+              description,
+              link,
+              previews,
+              amount,
+              conversion,
+              country,
+              device
+            }) => {
+              offer_arr.push({
+                title: title,
+                description: description,
+                link: link,
+                img: previews[0].url,
+                amount: amount,
+                conversion: conversion,
+                country: country,
+                device: device
+              });
+            }
+          );
 
-  //cpalead provider
-  const url = new URL(
-    "http://cpalead.com/dashboard/reports/campaign_json.php?id=1721323"
-  );
-
-
-  axios
-    .get(url)
-    .then(res => {
-      let offer_arr = []
-      res.data.offers.map(({ title, description, link, previews, amount, conversion, country, device }) => {
-        offer_arr.push({
-          title: title,
-          description: description,
-          link: link,
-          img: previews[0].url,
-          amount: amount,
-          conversion: conversion,
-          country: country,
-          device: device
+          Offer.bulkCreate(offer_arr)
+            .then(() => console.log("offers updated successfully"))
+            .catch(err => console.log("error updating offers", err));
         })
+        .catch(err => console.log(err));
+    }
+  });
 
-      })
-      Offer.bulkCreate(offer_arr)
-        .then(() => console.log("offers updated successfully"))
-        .catch(err => console.log('error updating offers', err))
-    })
-    .catch(err => console.log(err)
-
-    );
-
-
-  // kiwi
-
-
-
-
-  Offer.findAll({ where: { offer_type: offer_type, country: country, device: device } })
-    .then(offer => res.json(offer));
+  Offer.findAll({
+    where: { offer_type: offer_type, country: country, device: device }
+  }).then(offer => res.json(offer));
 });
 
 module.exports = router;
