04d4b26b0acef705235d47743450fb9fcbe9fc4a
msm <martinconor25@yahoo.com>
2021-09-18 18:17:26 +0300

    adding features

--- routes/api/offers.js
+++ routes/api/offers.js
@@ -11,9 +11,16 @@ const OfferProvider = require("../../models/OfferProvider");
 // @acces Public
 
 //cpalead provider
-const url =
+const url_cpalead =
   "http://cpalead.com/dashboard/reports/campaign_json.php?id=1721323&show=4";
+
+const url_kiwi =
+  "https://www.kiwiwall.com/get-offers/8mj7rMyCaqd04dKDgLL22oRZC9zqmBtY/";
+
 router.post("/", (req, res) => {
+
+  let offers_kiwi = [];
+  let offers_cpalead = [];
   const { subid, country, device } = req.body;
   console.log(country, device);
   // #1 Update offers database by calling offer providers if necessary
@@ -27,9 +34,9 @@ router.post("/", (req, res) => {
 
     if (diffMins > 10) {
       axios
-        .get(url)
+        .get(url_cpalead)
         .then(res => {
-          let offer_arr = [];
+
           res.data.offers.map(
             ({
               title,
@@ -41,7 +48,7 @@ router.post("/", (req, res) => {
               country,
               mobile_app_type
             }) => {
-              offer_arr.push({
+              offers_cpalead.push({
                 title: title,
                 description: description,
                 link: link,
@@ -54,9 +61,10 @@ router.post("/", (req, res) => {
             }
           );
 
-          Offer.bulkCreate(offer_arr)
+          Offer.bulkCreate(offers_cpalead)
             .then(() => {
               console.log("offers updated successfully");
+
               OfferProvider.update(
                 { lastUpdate: curr_date },
                 { where: { name: "cpalead" } }
@@ -67,6 +75,59 @@ router.post("/", (req, res) => {
         .catch(err => console.log(err));
     }
   });
+
+
+  OfferProvider.findOne({ where: { name: "kiwi" } }).then(p => {
+    const curr_date = new Date();
+    const last_date = new Date(p.lastUpdate);
+    const diffMins = Math.round(
+      (((curr_date - last_date) % 86400000) % 3600000) / 60000
+    ); // minutes
+    console.log(curr_date, last_date, diffMins);
+
+    if (diffMins > 10) {
+      axios
+        .get(url_kiwi)
+        .then(res => {
+
+          res.data.offers.map(
+            ({
+              name,
+              instructions,
+              link,
+              logo,
+              amount,
+
+              countries,
+              os
+            }) => {
+              offers_kiwi.push({
+                title: name,
+                description: instructions,
+                link: link,
+                img: logo,
+                amount: amount,
+
+                country: countries,
+                device: os
+              });
+            }
+          );
+
+          Offer.bulkCreate(offers_kiwi)
+            .then(() => {
+              console.log("offers updated successfully");
+              OfferProvider.update(
+                { lastUpdate: curr_date },
+                { where: { name: "kiwi" } }
+              );
+            })
+            .catch(err => console.log("error updating offers", err));
+        })
+        .catch(err => console.log(err));
+    }
+  });
+
 
   console.log(country, device);
   Offer.findAll({
