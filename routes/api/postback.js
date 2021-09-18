25f624857db95b3cc885972c90704f6a9971f073
msm <martinconor25@yahoo.com>
2021-09-18 17:45:51 +0300

    adding features

--- routes/api/postback.js
+++ routes/api/postback.js
@@ -6,51 +6,20 @@ const Postback = require("../../models/Postback");
 const User = require("../../models/User");
 const db = require("../../database");
 const cpalead_pass = "cpalead_123";
-const kiwi_pass = "kiwi_123";
+const kiwi_secret = "kiwi_123";
 // @route GET api/items
 // @desc Get All Items
 // @acces Public
-router.get("/", (req, res) => {
+router.get('/kiwi', (req, res) => {
+  const {
+    sub_id,
+    amount,
+    status,
+    offer_name,
+    signature
+  } = req.query;
+  if (signature === kiwi_secret) {
 
-  const { password } = req.query;
-
-  if (password === cpalead_pass) {
-
-    const {
-      subid,
-      payout
-    } = req.query;
-
-
-    const newPostback = Postback.build({
-      payout: `${payout}`,
-      subid: `${subid}`,
-      campaign_name: 'cpalead'
-    });
-
-    newPostback
-      .save()
-      .then(pb => console.log(success))
-      .catch(err => console.log(err));
-    // Update user
-
-    User.update(
-      { balance: db.literal(`balance + ${payout}`) },
-      { where: { id: subid } }
-    )
-      .then(() => console.log(`user balance updated ${payout}`))
-      .catch(err => console.log(err));
-
-    res.send("success");
-  }
-
-  else if (password === kiwi_pass) {
-    const {
-      sub_id,
-      amount,
-      status,
-      offer_name
-    } = req.query;
 
 
     const newPostback = Postback.build({
@@ -72,6 +41,45 @@ router.get("/", (req, res) => {
       { where: { id: sub_id } }
     )
       .then(() => console.log(`user balance updated ${amount}`))
+      .catch(err => console.log(err));
+
+    res.send("success");
+  }
+  else {
+
+    res.send("Not Authorized");
+    return;
+
+  }
+});
+
+
+router.get("/cpalead", (req, res) => {
+
+  const { password, subid, payout } = req.query;
+
+  if (password === cpalead_pass) {
+
+
+
+
+    const newPostback = Postback.build({
+      payout: `${payout}`,
+      subid: `${subid}`,
+      campaign_name: 'cpalead'
+    });
+
+    newPostback
+      .save()
+      .then(pb => console.log(success))
+      .catch(err => console.log(err));
+    // Update user
+
+    User.update(
+      { balance: db.literal(`balance + ${payout}`) },
+      { where: { id: subid } }
+    )
+      .then(() => console.log(`user balance updated ${payout}`))
       .catch(err => console.log(err));
 
     res.send("success");
