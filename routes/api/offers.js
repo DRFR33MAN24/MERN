e310be975a25297dc30c53331bcc284cb25939ae
Gordon Freeman <drfr33man@protonmail.com>
2021-09-20 10:11:13 +0300

    fixing something

--- routes/api/offers.js
+++ routes/api/offers.js
@@ -23,121 +23,126 @@ router.post("/", (req, res) => {
   const { subid, country, device } = req.body;
   console.log(country, device);
   // #1 Update offers database by calling offer providers if necessary
-  OfferProvider.findOne({ where: { name: "cpalead" } }).then(p => {
-    const curr_date = new Date();
-    const last_date = new Date(p.lastUpdate);
-    const diffMins = Math.round(
-      (((curr_date - last_date) % 86400000) % 3600000) / 60000
-    ); // minutes
-    console.log(curr_date, last_date, diffMins);
+  OfferProvider.findOne({ where: { name: "cpalead" } })
+    .then(p => {
+      const curr_date = new Date();
+      const last_date = new Date(p.lastUpdate);
+      const diffMins = Math.round(
+        (((curr_date - last_date) % 86400000) % 3600000) / 60000
+      ); // minutes
+      console.log(curr_date, last_date, diffMins);
 
-    if (diffMins > 10) {
-      axios
-        .get(url_cpalead)
-        .then(res => {
-          res.data.offers.map(
-            ({
-              title,
-              description,
-              link,
-              previews,
-              amount,
-              conversion,
-              country,
-              mobile_app_type
-            }) => {
-              offers_cpalead.push({
-                title: title,
-                description: description,
-                link: link,
-                img: previews[0].url,
-                amount: amount,
-                conversion: conversion,
-                country: country,
-                device: mobile_app_type
-              });
-            }
-          );
-          Offer.destroy({ where: { provider: "cpalead" } })
-            .then(n => console.log("number of deleted offers", n))
-            .catch(err => console.log(err));
-          Offer.bulkCreate(offers_cpalead)
-            .then(() => {
-              console.log("offers updated successfully");
+      if (diffMins > 10) {
+        axios
+          .get(url_cpalead)
+          .then(res => {
+            res.data.offers.map(
+              ({
+                title,
+                description,
+                link,
+                previews,
+                amount,
+                conversion,
+                country,
+                mobile_app_type
+              }) => {
+                offers_cpalead.push({
+                  title: title,
+                  description: description,
+                  link: link,
+                  img: previews[0].url,
+                  amount: amount,
+                  conversion: conversion,
+                  country: country,
+                  device: mobile_app_type,
+                  provider: "cpalead"
+                });
+              }
+            );
+            Offer.destroy({ where: { provider: "cpalead" } })
+              .then(n => console.log("number of deleted offers", n))
+              .catch(err => console.log(err));
+            Offer.bulkCreate(offers_cpalead)
+              .then(() => {
+                console.log("offers updated successfully");
 
-              OfferProvider.update(
-                { lastUpdate: curr_date },
-                { where: { name: "cpalead" } }
+                OfferProvider.update(
+                  { lastUpdate: curr_date },
+                  { where: { name: "cpalead" } }
+                );
+              })
+              .catch(err => console.log("error updating offers", err));
+          })
+          .catch(err => console.log(err));
+      }
+    })
+    .then(() => {
+      OfferProvider.findOne({ where: { name: "kiwi" } }).then(p => {
+        const curr_date = new Date();
+        const last_date = new Date(p.lastUpdate);
+        const diffMins = Math.round(
+          (((curr_date - last_date) % 86400000) % 3600000) / 60000
+        ); // minutes
+        console.log(curr_date, last_date, diffMins);
+
+        if (diffMins > 10) {
+          axios
+            .get(url_kiwi)
+            .then(res => {
+              res.data.offers.map(
+                ({
+                  name,
+                  instructions,
+                  link,
+                  logo,
+                  amount,
+
+                  countries,
+                  os
+                }) => {
+                  offers_kiwi.push({
+                    title: name,
+                    description: instructions,
+                    link: link,
+                    img: logo,
+                    amount: amount,
+
+                    country: countries,
+                    device: os,
+                    provider: "kiwi"
+                  });
+                }
               );
+              Offer.destroy({ where: { provider: "kiwi" } })
+                .then(n => console.log("number of deleted offers", n))
+                .catch(err => console.log(err));
+              Offer.bulkCreate(offers_kiwi)
+                .then(() => {
+                  console.log("offers updated successfully");
+                  OfferProvider.update(
+                    { lastUpdate: curr_date },
+                    { where: { name: "kiwi" } }
+                  );
+                })
+                .catch(err => console.log("error updating offers", err));
             })
-            .catch(err => console.log("error updating offers", err));
-        })
-        .catch(err => console.log(err));
-    }
-  });
-
-  OfferProvider.findOne({ where: { name: "kiwi" } }).then(p => {
-    const curr_date = new Date();
-    const last_date = new Date(p.lastUpdate);
-    const diffMins = Math.round(
-      (((curr_date - last_date) % 86400000) % 3600000) / 60000
-    ); // minutes
-    console.log(curr_date, last_date, diffMins);
-
-    if (diffMins > 10) {
-      axios
-        .get(url_kiwi)
-        .then(res => {
-          res.data.offers.map(
-            ({
-              name,
-              instructions,
-              link,
-              logo,
-              amount,
-
-              countries,
-              os
-            }) => {
-              offers_kiwi.push({
-                title: name,
-                description: instructions,
-                link: link,
-                img: logo,
-                amount: amount,
-
-                country: countries,
-                device: os
-              });
-            }
-          );
-          Offer.destroy({ where: { provider: "kiwi" } })
-            .then(n => console.log("number of deleted offers", n))
             .catch(err => console.log(err));
-          Offer.bulkCreate(offers_kiwi)
-            .then(() => {
-              console.log("offers updated successfully");
-              OfferProvider.update(
-                { lastUpdate: curr_date },
-                { where: { name: "kiwi" } }
-              );
-            })
-            .catch(err => console.log("error updating offers", err));
-        })
-        .catch(err => console.log(err));
-    }
-  });
-
-  console.log(country, device);
-  Offer.findAll({
-    where: { country: country, device: device },
-    raw: true,
-    nest: true
-  }).then(offer => {
-    //loop on all offers and change subid
-    console.log(offer);
-    res.json(offer);
-  });
+        }
+      });
+    })
+    .then(() => {
+      console.log(country, device);
+      Offer.findAll({
+        where: { country: country, device: device },
+        raw: true,
+        nest: true
+      }).then(offer => {
+        //loop on all offers and change subid
+        console.log(offer);
+        res.json(offer);
+      });
+    });
 });
 
 module.exports = router;
