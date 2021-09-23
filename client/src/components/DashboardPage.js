1340981b99adccaee6ecb657eb93795a3aab2cba
Gordon Freeman <drfr33man@protonmail.com>
2021-09-23 09:30:11 +0300

    adding new features

--- client/src/components/DashboardPage.js
+++ client/src/components/DashboardPage.js
@@ -49,7 +49,7 @@ class DashboardPage extends Component {
       const { user } = this.props;
       axios.get("https://freegeoip.app/json/").then(res => {
         const country = res.data.country_code.toLowerCase();
-        this.props.getOffers(user.id, "us", "ios");
+        this.props.getOffers(user.id, country, "android");
       });
     }
 
@@ -128,7 +128,7 @@ class DashboardPage extends Component {
     //const { user } = this.props;
     // console.log(user.id);
     const isAuthenticated = this.props.isAuthenticated;
-    console.log(offers);
+    // console.log(offers);
     //console.log("dashboard render is Auth", this.props.isAuthenticated);
     const login = <Redirect exact to="/Login" />;
     if (!isAuthenticated) {
