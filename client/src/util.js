0c17273077e5b3d266167e4ac3ece0c2363ed0ba
Gordon Freeman <drfr33man@protonmail.com>
2021-09-23 09:40:26 +0300

    adding new features

--- client/src/util.js
+++ client/src/util.js
@@ -1,9 +1,33 @@
-
 const toDollars = cents => {
+  var dollars = cents / 100;
+  dollars = dollars.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
+  return dollars;
+};
 
-    var dollars = cents / 100;
-    dollars = dollars.toLocaleString("en-US", { style: "currency", currency: "USD" });
-    return dollars;
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+    platform = window.navigator.platform,
+    macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+    windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+    iosPlatforms = ["iPhone", "iPad", "iPod"],
+    os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = "Mac OS";
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = "ios";
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = "Windows";
+  } else if (/Android/.test(userAgent)) {
+    os = "android";
+  } else if (!os && /Linux/.test(platform)) {
+    os = "Linux";
+  }
+
+  return os;
 }
 
-module.exports = { toDollars }
+module.exports = { toDollars, getOS };
