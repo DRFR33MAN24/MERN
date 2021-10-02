f15801060f7f8567ca226d70878c7fbd3c888fd0
Gordon Freeman <drfr33man@protonmail.com>
2021-10-02 17:48:19 +0300

    fix error

--- server.js
+++ server.js
@@ -31,19 +31,14 @@ app.use("/api/email", require("./routes/api/email"));
 app.use("/api/stats", require("./routes/api/stats"));
 app.use("/api/contact", require("./routes/api/contact"));
 
-//app.use(express.static("app"));
-app.get("/app/*", function(req, res) {
-  res.sendfile("/app/index.html");
-});
 // Serve static assets if in production
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+app.use(express.static("build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  console.log(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 
