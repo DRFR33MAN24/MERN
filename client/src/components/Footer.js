b6861537a0a4973019a8fd94b219346bdaea3537
Gordon Freeman <drfr33man@protonmail.com>
2021-10-13 22:08:53 +0300

    added image to surveys

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 
-
 export default class Footer extends Component {
-
-    render() {
-        return (
-            <div>
-                <footer className="bg-dark text-white">
-                    <small>This is a footer</small>
-                </footer>
+  render() {
+    return (
+      <div>
+        <footer className="bg-dark text-white">
+          <div className="container d-flex justify-content-center">
+            <div className="row">
+              <div className="col-sm"></div>
+              <div className="col"></div>
             </div>
-        );
-    }
+            <div className="row">
+              <div className="footer-copyright text-center py-3">
+                <div className="container-fluid">
+                  &copy; {new Date().getFullYear()} Copyright:{" "}
+                  <a href="https://www.coinguru.biz"> CoinGuru.biz </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
