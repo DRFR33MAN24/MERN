dfca68ad93465cb81aaf61de5090ac5c0b2adf57
Gordon Freeman <drfr33man@protonmail.com>
2021-10-19 01:35:45 +0300

    change cards layout

--- client/src/components/WithdrawModals/CardTypes.js
+++ client/src/components/WithdrawModals/CardTypes.js
@@ -4,17 +4,41 @@ export const cards = [
     img: "./gift_img/google.png",
     varients: [
       { type: "Google Play USA 5$", amount: 500 },
-      { type: "Google Play Canada 15$", amount: 1500 },
-      { type: "Google Play India 10$", amount: 1000 }
+      { type: "Google Play Australia 5$", amount: 500 },
+      { type: "Google Play India 5$", amount: 500 },
+      { type: "Google Play Germany 5$", amount: 500 },
+      { type: "Google Play France 5$", amount: 500 },
+      { type: "Google Play United Kingdom 5$", amount: 500 },
+      { type: "Google Play UAE 5$", amount: 500 },
+      { type: "Google Play Mexico 5$", amount: 500 },
+      { type: "Google Play Spain 5$", amount: 500 },
+      { type: "Google Play Turkey 5$", amount: 500 },
+      { type: "Google Play Netherlands 5$", amount: 500 },
+      { type: "Google Play Brazil 5$", amount: 500 },
+      { type: "Google Play Belgium 5$", amount: 500 },
+      { type: "Google Play KSA 5$", amount: 500 },
+      { type: "Google Play Finland 5$", amount: 500 },
+      { type: "Google Play Greece 5$", amount: 500 },
+      { type: "Google Play Switzerland 5$", amount: 500 },
+      { type: "Google Play Korea 5$", amount: 500 },
+      { type: "Google Play Austria 5$", amount: 500 },
+      { type: "Google Play Ireland 5$", amount: 500 },
+      { type: "Google Play Portugal 5$", amount: 500 }
     ]
   },
+
   {
     name: "Razer Gold Card",
     img: "./gift_img/razer-gold.png",
     varients: [
       { type: "Razer Gold USA 5$", amount: 500 },
-      { type: "Razer Gold Canada 15$", amount: 1500 },
-      { type: "Razer Gold India 10$", amount: 1000 }
+      { type: "Razer Gold Singapore 5$", amount: 500 },
+      { type: "Razer Gold UAE 5$", amount: 500 },
+      { type: "Razer Gold Turkey 5$", amount: 500 },
+      { type: "Razer Gold Thailand 5$", amount: 500 },
+      { type: "Razer Gold Taiwan 5$", amount: 500 },
+      { type: "Razer Gold Hong Kong 5$", amount: 500 },
+      { type: "Razer Gold Indonesia 5$", amount: 500 }
     ]
   }
 ];
