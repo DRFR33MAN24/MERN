3629c2be0f3fee721b176ab715d00c28ff94fd51
Gordon Freeman <drfr33man@protonmail.com>
2021-10-22 13:28:17 +0300

    fix download csv

--- client/src/components/WithdrawModals/CardTypes.js
+++ client/src/components/WithdrawModals/CardTypes.js
@@ -2,40 +2,46 @@ export const cards = [
   {
     name: "Google Play Card",
     img: "./gift_img/google.png",
-    varients: [
-      { type: "Google Play USA", amounts: [500, 1000, 1500, 2000] },
-      { type: "Google Play India", amounts: [500, 1000, 1500] }
-    ]
+    varients: [{ type: "Google Play USA", amounts: [500, 1000, 1500, 2000] }]
+  },
+  {
+    name: "Razer Gold Card",
+    img: "./gift_img/razer-gold.png",
+    varients: [{ type: "Razer Gold USA", amounts: [1000, 1500, 2000] }]
+  },
+  {
+    name: "Apple USA",
+    img: "./gift_img/apple-gift.png",
+    varients: [{ type: "Apple USA", amounts: [500, 1000, 1500, 2000] }]
+  },
+  {
+    name: "Roblox USD",
+    img: "./gift_img/roblox.png",
+    varients: [{ type: "Roblox USD", amounts: [1000, 2000] }]
   }
 ];
 
-// {
-//   name: "Google Play Card",
-//   img: "./gift_img/google.png",
-//   varients: [
-//     { type: "Google Play USA 5$", amount: 500 },
-//     { type: "Google Play Australia 5$", amount: 500 },
-//     { type: "Google Play India 5$", amount: 500 },
-//     { type: "Google Play Germany 5$", amount: 500 },
-//     { type: "Google Play France 5$", amount: 500 },
-//     { type: "Google Play United Kingdom 5$", amount: 500 },
-//     { type: "Google Play UAE 5$", amount: 500 },
-//     { type: "Google Play Mexico 5$", amount: 500 },
-//     { type: "Google Play Spain 5$", amount: 500 },
-//     { type: "Google Play Turkey 5$", amount: 500 },
-//     { type: "Google Play Netherlands 5$", amount: 500 },
-//     { type: "Google Play Brazil 5$", amount: 500 },
-//     { type: "Google Play Belgium 5$", amount: 500 },
-//     { type: "Google Play KSA 5$", amount: 500 },
-//     { type: "Google Play Finland 5$", amount: 500 },
-//     { type: "Google Play Greece 5$", amount: 500 },
-//     { type: "Google Play Switzerland 5$", amount: 500 },
-//     { type: "Google Play Korea 5$", amount: 500 },
-//     { type: "Google Play Austria 5$", amount: 500 },
-//     { type: "Google Play Ireland 5$", amount: 500 },
-//     { type: "Google Play Portugal 5$", amount: 500 }
-//   ]
-// },
+// ,
+//       { type: "Google Play United Kingdom", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Australia", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Germany", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play France", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play UAE", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Mexico", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Spain", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Turkey", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Netherlands", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Brazil", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Belgium", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play KSA", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Finland", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Greece", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Switzerland", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Korea", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Austria", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Ireland", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play Portugal", amounts: [500, 1000, 1500, 2000] },
+//       { type: "Google Play India", amounts: [500, 1000, 1500] }
 
 // ,
 // {
