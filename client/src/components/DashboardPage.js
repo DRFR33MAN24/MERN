0f1aa9b9373798e00de3904d15c087f1ed170adb
Gordon Freeman <drfr33man@protonmail.com>
2021-10-25 17:39:47 +0300

    add cosmetic features

--- client/src/components/DashboardPage.js
+++ client/src/components/DashboardPage.js
@@ -72,8 +72,8 @@ class DashboardPage extends Component {
     const clientOS = getOS();
     axios.get("https://freegeoip.app/json/").then(res => {
       const country = res.data.country_code.toLowerCase();
-      //this.props.getOffers(user.id, country, clientOS); //Test_cond
-      this.props.getOffers(user.id, "us", "android");
+      this.props.getOffers(user.id, country, clientOS);
+      // this.props.getOffers(user.id, "us", "android");
     });
   }
 
